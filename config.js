56359bed2a6ca2dff5c3bf76d239e6e81e84aaa8
sacibaby <111527520+sacibaby@users.noreply.github.com>
2022-09-10 13:32:28 +0530

    Update config.js
    Signed-off-by: sacibaby <111527520+sacibaby@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,13 +37,13 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
-global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
-global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
-global.packname = 'ZIM BOT INC'
+global.owner = ['94770378874','']
+global.premium = ['94770378874','94768440031']
+global.botname = 'ZIM BOT INC SRI-LANKA'
+global.ownername = 'SUNETH'
+global.group1 = 'https://chat.whatsapp.com/Dhq8wi5q82zF76VK8tzaeO'
+global.group2 = 'http://wa.me/+94770378874'
+global.packname = 'ZIM BOT SUNETH'
 global.ytchannel = 'https://youtu.be/ww4z2m3uORU'
 global.monayawal = '100000'
 global.darahawal = '100'
